d4dfe5416bea6f774bd477bd693eccd980b5bf91
Brandon Toone <brandon@rdlk.co>
2022-03-17 00:44:04 -0600

    Define serialization for Registration

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -2,4 +2,17 @@
 
 class Registration < ApplicationRecord
   validates :username, uniqueness: true
+
+  # Returns a hash, that can be serialized into a JSON string
+  def as_json(options = {})
+    {
+      'username' => @username,
+      'address' => @address
+    }
+  end
+
+  # Stores as a json string
+  def to_json(*options)
+    as_json(*options).to_json(*options)
+  end
 end
--- spec/models/registration_spec.rb
+++ spec/models/registration_spec.rb
@@ -10,4 +10,21 @@ RSpec.describe Registration, type: :model do
       expect(duplicate).not_to be_valid
     end
   end
+
+  describe '#as_json' do
+    let(:registration) { FactoryBot.create :registration }
+
+    it 'does not include timestamps' do
+      expect(registration.as_json).not_to include('id', 'created_at', 'updated_at')
+    end
+  end
+
+  describe '#to_json' do
+    let(:registration) { FactoryBot.create :registration }
+
+    it 'returns a JSON string' do
+      actual = registration.to_json
+      expect(JSON(actual)).to eq registration.as_json
+    end
+  end
 end
