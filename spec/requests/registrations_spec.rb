d8dbdcee2b4ddcef4c99680c3dd40aee47467ae0
Brandon Toone <brandon@rdlk.co>
2022-03-18 13:57:50 -0600

    Fix failing spec
    Adding the sequence in factory caused this failure.

--- spec/requests/registrations_spec.rb
+++ spec/requests/registrations_spec.rb
@@ -24,6 +24,8 @@ RSpec.describe 'Registrations', type: :request do
     end
 
     context 'with an invalid username' do
+      let(:registration_params) { FactoryBot.attributes_for :registration, username: Registration.first.username }
+
       before do
         FactoryBot.create :registration
       end
